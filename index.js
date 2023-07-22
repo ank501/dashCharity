10776b98489158aa0af21f1f5d6e85fb1ac7e58f
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-07-22 19:17:42 +0530

    admin added

--- index.js
+++ index.js
@@ -13,13 +13,8 @@ const userDetailsRoute = require("./routes/admin/userDetailsRoute");
 const organizationRouter = require("./routes/organizationRoute");
 const paymentRouter = require("./routes/paymentRoute");
 app.use(express.json());
-// app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(cors());
+
 app.use('/users', userRouter);
 app.use('/donation', donationRouter);
 app.use('/admin', adminRouter)
