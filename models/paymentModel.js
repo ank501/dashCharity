dd5ab79535488e81be30c3a21fefc729bfb7564c
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-24 14:55:23 +0530

    code added

--- models/paymentModel.js
+++ models/paymentModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const paymentSchema = new mongoose.Schema({
-  user: {
+  userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
     required: true
