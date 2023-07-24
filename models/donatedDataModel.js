dd35ddb5a4618788e30298755f1a1f694e7803ac
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-24 13:40:56 +0530

    code added

--- models/donatedDataModel.js
+++ models/donatedDataModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const donatedDataSchema = new mongoose.Schema({
+    userId : {type : mongoose.Schema.Types.ObjectId, ref : 'user', required : true},
     name : {type : String, required : true},
     amount : {type : Number, required : true},
     message : {type : String, required : true},
