c13d11aacd168674ebd702d2c7b46bf9f5627369
ritesh22201 <riteshgoswami22201@gmail.com>
2023-08-30 08:34:56 +0530

    code added

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -8,49 +8,47 @@ const crypto = require('crypto');
 
 // Payment Request
 
-paymentRouter.post('/orders', auth, async(req, res) => {
+paymentRouter.post('/orders', auth, async (req, res) => {
     try {
         const instance = new Razorpay({
-            key_id : process.env.razorPayKey,
-            key_secret : process.env.razorPaySecret
+            key_id: process.env.razorPayKey,
+            key_secret: process.env.razorPaySecret
         })
 
         const options = {
-            amount : req.body.amount*100,
-            currency : "USD",
-            receipt : crypto.randomBytes(10).toString("hex")
+            amount: req.body.amount * 100,
+            currency: "USD",
+            receipt: crypto.randomBytes(10).toString("hex")
         }
 
         instance.orders.create(options, (error, order) => {
-            if(error){
-                res.status(400).send({msg : 'Something went wrong!'})
+            if (error) {
+                res.status(400).send({ msg: 'Something went wrong!' })
             }
-            else{
-                res.status(200).send({data : order});
+            else {
+                res.status(200).send({ data: order });
             }
         })
     } catch (error) {
-        res.status(400).send({'msg' : error.message});
+        res.status(400).send({ 'msg': error.message });
     }
 })
 
 // Payment verification
 
-paymentRouter.post('/verify', async(req, res) => {
+paymentRouter.post('/verify', async (req, res) => {
     try {
-        const {razorpay_order_id, razorpay_payment_id, razorpay_signature} = req.body;
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
         const sign = `${razorpay_order_id} | ${razorpay_payment_id}`;
         const userSign = crypto.createHmac('sha256', process.env.razorPaySecret).update(sign.toString()).digest('hex');
 
-        if(razorpay_signature === userSign){
-            res.status(200).send({msg : 'Payment Successful'});
+        if (razorpay_signature === userSign) {
+            return res.status(200).send({ msg: 'Payment Successful' });
         }
-        else{
-            res.status(400).send({msg : 'Invalid credentials!'})
-        }
+        res.status(400).send({ msg: 'Invalid credentials!' })
 
     } catch (error) {
-        res.status(400).send({msg : error.message});
+        res.status(400).send({ msg: error.message });
     }
 })
 
